e4be45fab7192bd9452be50721de49d8ada149d7
Fabio Perfetti <perfabio87@gmail.com>
2014-12-01 00:28:25 +0100

    Refactored with object

--- calculator/calc/models.py
+++ calculator/calc/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+class Calculator(models.Model):
+    OPERATIONS = (
+        ('sum', 'sum'),
+        ('min', 'min'),
+        ('mul', 'mul'),
+        ('div', 'div'),
+        ('equ', 'equ'),
+    )
+    valueA = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    valueB = models.DecimalField(max_digits=10, decimal_places=3, default=0)
+    operation = models.CharField(max_length=3, choices=OPERATIONS, )
+
+    def evaluate(self):
+        if self.operation == 'equ':
+            return (float(self.valueA), None)
+        elif self.operation == 'sum':
+            return (float(self.valueA + self.valueB), None)
+        elif self.operation == 'min':
+            return (float(self.valueA - self.valueB), None)
+        elif self.operation == 'mul':
+            return (float(self.valueA * self.valueB), None)
+        elif self.operation == 'div':
+            if self.valueB == 0:
+                print("Division by zero")
+                return (None, 'Division by zero')
+            return (float(self.valueA / self.valueB), None)
+        return 0
--- calculator/calc/views.py
+++ calculator/calc/views.py
@@ -2,110 +2,34 @@ from django.http import HttpResponse
 
 # Create your views here.
 from django.template import RequestContext, loader
-
-# def index(request):
-# init = request.GET.get('init',True)
-# print('init: %s' % init)
-#
-#     currVal = float( request.GET.get('input',0) )
-#     print('curVal: %s' % currVal)
-#
-#     op = request.GET.get('op',None)
-#     print('op: %s' % op)
-#
-#     if init is False:
-#         showTotal=True
-#         showCurr=True
-#     else:
-#         showTotal=False
-#         showCurr=False
-#
-#     total = 0
-#
-#     template = loader.get_template('index.html')
-#     context = RequestContext(request, {
-#         'init': init,
-#         'showTotal': showTotal,
-#         'total': str(total),
-#         'showTotal': showCurr,
-#         'curr' : str(currVal),
-#         'op' : op,
-#     })
-#     return HttpResponse(template.render(context))
-#
+from models import Calculator
 from forms import CalculatorForm
 
 
-def index2(request):
-    step = request.GET.get('step', '1')
-    currVal = float(request.GET.get('input', 0))
-    print(currVal)
-    currOp = request.GET.get('op', None)
-    print(currOp)
-    total = 0
-
-    print('step: %s' % step)
-    if step == '1':  # show just the buttons w/o any label
-        showVal = False
-        showTotal = False
-        request.session['preVal'] = 0
-        step = 2
-    elif step == '2':  # show selected value and op ... waiting for the second value
-        showVal = True
-        showTotal = False
-        request.session['preVal'] = currVal
-        request.session['op'] = currOp
-        step = 3
-    elif step == '3':  # show previous value, op and total; if operation not 'equals' then allows user to digit new value
-        showVal = True
-        showTotal = True
-        total = doOp(request.session['op'], float(request.session['preVal']), currVal)
-        if currOp == 'eq':
-            step = 0
-            showVal = False
-            showTotal = True
-            print("finished")
-        else:
-            step = 3
-            request.session['preVal'] = total
-            request.session['op'] = currOp
-
-    template = loader.get_template('index.html')
-    context = RequestContext(request, {
-        'step': step,
-        'showVal': showVal,
-        'val': currVal,
-        'op': currOp,
-        'showTotal': showTotal,
-        'total': total
-
-    })
-    return HttpResponse(template.render(context))
-
-
 def index(request):
-    total=None
-    step=None
-    error=None
-    form=None
+    total = None
+    step = None
+    error = None
+    form = None
 
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = CalculatorForm(request.POST)
         # check whether it's valid:
         if form.is_valid():
-            step=form.cleaned_data['step']
-            input=float(form.cleaned_data['input'])
-            op= form.cleaned_data['op']
+            # retrieving data
+            print("Form is Valid")
+            step = form.cleaned_data['step']
+            input = float(form.cleaned_data['input'])
+            op = form.cleaned_data['op']
             step, total, error = validForm(request, step, input, op)
     else:
         # initialize variables
         step, total, error = reset(request)
 
-
     template = loader.get_template('index.html')
     context = RequestContext(request, {
-        'form' : form,
+        'form': form,
         'error': error,
         'total': total,
         'step': step,
@@ -113,54 +37,48 @@ def index(request):
     return HttpResponse(template.render(context))
 
 
-def validForm(request, step, input, op):
-    error=None
-    if op == 'clc':
-        step, total, error = reset(request)
-    elif op == 'equ':
-        if request.session['op'] and request.session['preVal']:
-            total, error = doOp(request.session['op'], request.session['preVal'], float(input))
-        else:
-            total = input
-        step=1
-    else:
-        if step == 1:      # no total
-            request.session['preVal'] = float(input)
-            request.session['op'] = op
-            total=None
-            step=2
-        elif step == 2:    # there is total
-            total,  error = doOp(request.session['op'], request.session['preVal'], input)
-            request.session['preVal'] = total
-            if op != 'equ':
-                request.session['op'] = op
-            step=2
-    return (step, total, error)
-
-
 def reset(request):
-    error=None
-    total=None
-    step=1
-    request.session['preVal'] = float(0)
-    request.session['op'] = None
+    error = None
+    total = None
+    step = 1
+    request.session['calculator'] = None
     return (step, total, error)
 
 
-
-def doOp(op, a, b):
-    print("operating...")
-    if op == 'sum':
-        return (a + b, None)
-    elif op == 'min':
-        return (a - b, None)
-    elif op == 'mul':
-        return (a * b, None)
-    elif op == 'div':
-        if b==0:
-            print("Division by zero")
-            return (None, 'Division by zero')
-        return (float(a / b), None)
-    return 0
+def validForm(request, step, input, op):
+    error = None
+    total = None
+    if op == 'clc':
+        request.session['calculator'] = None
+        total = None
+        step = 1
+    elif step == 1:
+        if op == 'equ':
+            calc = Calculator(valueA=float(input), operation=op)
+            total = calc.evaluate()
+            step = 1
+        else:
+            request.session['valueA'] = input
+            request.session['operation'] = op
+            step = 2
+    elif step == 2:
+        if op == 'equ':
+            valueA = request.session.get('valueA', 0)
+            op = request.session['operation']
+            calc = Calculator(valueA=float(valueA), valueB=float(input), operation=op)
+            total, error = calc.evaluate()
+            request.session['valueA'] = total
+            print(total)
+            step = 1
+        else:
+            op = request.session['operation']
+            valueA = request.session['valueA']
+            calc = Calculator(valueA=float(valueA), valueB=float(input), operation=op)
+            total, error = calc.evaluate()
+            request.session['valueA'] = total
+            request.session['operation'] = op
+            print(total)
+            step = 2
+    return (step, total, error)
 
 
